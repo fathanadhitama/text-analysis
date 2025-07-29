1738f2d46e53b384d5b14b09919cb0562cecb13d
Fathan <fathan.naufal@ui.ac.id>
2025-07-29 11:54:13 +0700

    feat: setup action workflow

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ from etl.clean import clean_task
 from etl.scrape_detik import scrape_detik
 from etl.to_bigquery import push_to_bigquery
 import os
+from dotenv import load_dotenv
 
-def main(request):
-    # Ambil env langsung dari Cloud Function, bukan .env
-    # google_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    # if google_credentials:
-    #     print("✅ GOOGLE_APPLICATION_CREDENTIALS loaded")
-    # else:
-    #     print("❌ GOOGLE_APPLICATION_CREDENTIALS not found")
+load_dotenv()
+# Ensure GOOGLE_APPLICATION_CREDENTIALS is set from .env
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
-    print("🚀 Starting sentiment analysis pipeline...")
-    raw_df = scrape_detik()
-    cleaned_df = clean_task(raw_df)
-    final_df = sentiment_task(cleaned_df)
+GOOGLE_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+if GOOGLE_CREDENTIALS:
+    print(f"✅ GOOGLE_APPLICATION_CREDENTIALS loaded")
+else:
+    print("❌ GOOGLE_APPLICATION_CREDENTIALS not found in .env or is empty")
 
-    push_to_bigquery(
-        final_df,
-        project_id="compnet-2206825965",
-        dataset_id="news_analysis",
-        table_id="sentiment_results"
-    )
+# Main execution flow
+print("Starting sentiment analysis pipeline...")
+raw_df = scrape_detik()
+cleaned_df = clean_task(raw_df)
+final_df = sentiment_task(cleaned_df)
 
-    return "✅ ETL pipeline finished successfully", 200
+push_to_bigquery(
+    final_df,
+    project_id="compnet-2206825965",
+    dataset_id="news_analysis",
+    table_id="sentiment_results"
+)
